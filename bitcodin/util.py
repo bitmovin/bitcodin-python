51fd9e8de426988c90c5ae02e08af8e243808037
Dominic Miglar <dominic.miglar@w1r3.net>
2015-06-29 15:01:47 +0200

    added python3 support to bitcodin-python

--- bitcodin/util.py
+++ bitcodin/util.py
@@ -2,14 +2,13 @@ __author__ = 'David Moser <david.moser@bitmovin.net>'
 
 import re
 
-
 def convert_dict(d):
     new_d = {}
-    for k, v in d.iteritems():
+    for k, v in d.items():
         new_d[convert(k)] = convert_dict(v) if isinstance(v, dict) else v
     return new_d
-
 
 def convert(name):
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
