ef8b3e1b4ee01b9728405733a128f22934d8b1e1
Dominic Miglar <dominic.miglar@w1r3.net>
2015-07-02 09:18:19 +0200

    added example create_job.py

---
+++ examples/create_job.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import bitcodin
+
+bitcodin.api_key = 'YOUR API KEY'
+
+input_obj = bitcodin.Input(url='http://eu-storage.bitcodin.com/inputs/Sintel.2010.720p.mkv')
+input_result = bitcodin.create_input(input_obj)
+
+video_configs = []
+video_configs.append(bitcodin.VideoStreamConfig(default_stream_id=0, bitrate=1024000, profile='Main',
+    preset='standard', height=1024, width=768))
+video_configs.append(bitcodin.VideoStreamConfig(default_stream_id=1, bitrate=512000, profile='Main',
+    preset='standard', height=480, width=640))
+
+audio_configs = [bitcodin.AudioStreamConfig(default_stream_id=0, bitrate=192000)]
+
+encoding_profile_obj = bitcodin.EncodingProfile('API Test Profile', video_configs, audio_configs)
+encoding_profile_result = bitcodin.create_encoding_profile(encoding_profile_obj)
+
+manifests = ['mpd', 'm3u8']
+
+job = bitcodin.Job(input_result.input_id, encoding_profile_result.encoding_profile_id, manifests)
+job_result = bitcodin.create_job(job)
