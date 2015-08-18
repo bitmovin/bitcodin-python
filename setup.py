cdb27ca54b0e2b8611f79b2029a9292716d0dad1
Dominic Miglar <dominic.miglar@w1r3.net>
2015-08-18 12:42:28 +0200

    Version 0.5.0 - HLS Encryption support, multiple audio streams feature

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='bitcodin',
     description='Python interface for bitcodin API',
-    version='0.4.0',
+    version='0.5.0',
     author='David Moser, Dominic Miglar',
     author_email='david.moser@bitmovin.net, dominic.miglar@bitmovin.net',
     packages=['bitcodin'],
