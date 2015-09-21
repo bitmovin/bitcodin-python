e4b05b1224ad1dafe107143e9f3b9fe89b78ef43
Guenther Cwioro <guenther.cwioro@bitmovin.net>
2015-09-21 15:00:19 +0200

    Fix: namespace error.

--- bitcodin/test/job/testcase_transfer_job_s3.py
+++ bitcodin/test/job/testcase_transfer_job_s3.py
@@ -67,7 +67,7 @@ class TransferJobToS3TestCase(BitcodinTestCase):
 
 
     def runTest(self):
-        start_time = time.time()
+        start_time = time()
         time_limit = 600
         while(True):
             job_status = get_job_status(self.job.job_id)
