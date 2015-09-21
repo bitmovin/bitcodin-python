cb96455a781733077995c2578cf1cd08954932de
Guenther Cwioro <guenther.cwioro@bitmovin.net>
2015-09-21 14:49:43 +0200

    Add time limit for job updates.

--- bitcodin/test/config.py
+++ bitcodin/test/config.py
@@ -3,5 +3,5 @@ __author__ = 'Dominic Miglar <dominic.miglar@bitmovin.net>'
 # bitcodin-python test configuration
 # Please do not place sensitive information here -> for sensitive information see settings.py (Encrypt for travis tests!)
 
-test_video_url = 'http://bitbucketireland.s3.amazonaws.com/h264_720p_mp_3.1_3mbps_aac_shrinkage.mp4'
+test_video_url = 'http://bitbucketireland.s3.amazonaws.com/Sintel-original-short.mkv'
 test_video_url_multiple_audio_streams = 'http://bitbucketireland.s3.amazonaws.com/Sintel-two-audio-streams-short.mkv'
--- bitcodin/test/job/testcase_transfer_job_ftp.py
+++ bitcodin/test/job/testcase_transfer_job_ftp.py
@@ -1,7 +1,7 @@
 __author__ = 'Dominic Miglar <dominic.miglar@bitmovin.net>'
 
 import unittest
-from time import sleep
+from time import sleep, time
 from bitcodin import create_job
 from bitcodin import create_input
 from bitcodin import create_encoding_profile
@@ -61,12 +61,16 @@ class TransferJobToFTPTestCase(BitcodinTestCase):
 
 
     def runTest(self):
+        start_time = time.time()
+        time_limit = 600
         while(True):
             job_status = get_job_status(self.job.job_id)
             if(job_status.status.lower() == 'finished'):
                 break
             elif(job_status.status.lower() == 'error'):
                 raise BitcodinError('An error occured while waiting for job to be FINISHED', 'Job status changed to ERROR!')
+            elif(time.time() - start_time > time_limit):
+                raise BitcodinError('Timeout of job duration exceeded!', 'Job took too long!')
             sleep(2)
         transfer = transfer_job(self.job.job_id, self.output.output_id)
 
