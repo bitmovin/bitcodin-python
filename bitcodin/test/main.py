32fbb816ac094682e929750cfb87a8939bede66e
Guenther Cwioro <guenther.cwioro@bitmovin.net>
2015-09-23 14:35:11 +0200

    Execute tests parallel.

--- bitcodin/test/main.py
+++ bitcodin/test/main.py
@@ -1,3 +1,9 @@
+import pprint
+from testtools import ConcurrentStreamTestSuite, StreamResult, TextTestResult, StreamFailFast, TestResult, StreamToDict, \
+    StreamToExtendedDecorator, StreamSummary, CopyStreamResult
+import time
+import bitcodin
+
 __author__ = 'Dominic Miglar <dominic.miglar@bitmovin.net>'
 
 import sys
@@ -28,17 +34,38 @@ def _collect_test_suites():
 def _run_tests(test_suite):
     return TextTestRunner(verbosity=VERBOSITY_LEVEL).run(test_suite)
 
+def callback(arg):
+    if "traceback" in arg["details"]:
+        traceback = arg["details"]['traceback'].as_text()
+        global FAILED
+        FAILED = True
+    else:
+        traceback = ""
+
+    print(arg["status"] + " " + traceback)
+
+
 
 def main():
+
     test_suites = _collect_test_suites()
     main_test_suite = TestSuite()
 
     for test_suite in test_suites:
         main_test_suite.addTests(test_suite)
 
-    test_suite_result = _run_tests(main_test_suite)
-    if not test_suite_result.wasSuccessful():
+    concurrent_suite = ConcurrentStreamTestSuite(lambda: ((case, None) for case in main_test_suite))
+
+    result = StreamToDict(callback)
+    result.startTestRun()
+    try:
+        concurrent_suite.run(result)
+    finally:
+        result.stopTestRun()
+
+    if 'FAILED' in globals() and FAILED:
         sys.exit(1)
+
 
 if __name__ == '__main__':
     main()
