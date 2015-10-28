5fc36eb2f25a09167cac41518b76276be0be580b
David Moser <david.moser@perau.at>
2015-10-28 16:01:31 +0100

    testcase_create_delete_live_stream.py: Adjusted test to create it's own output and encoding profile setup.py: Changed version to 1.0.0

--- bitcodin/test/live_stream/testcase_create_delete_live_stream.py
+++ bitcodin/test/live_stream/testcase_create_delete_live_stream.py
@@ -5,37 +5,43 @@ from bitcodin.test.bitcodin_test_case import BitcodinTestCase
 
 import bitcodin
 import time
+from bitcodin import EncodingProfile
+from bitcodin import AudioStreamConfig
+from bitcodin import VideoStreamConfig
+from bitcodin import create_encoding_profile
+from bitcodin import GCSOutput
+from bitcodin.test.settings import gcs_output_config
 
 
 class CreateLiveStreamTestCase(BitcodinTestCase):
 
     def setUp(self):
+        output = GCSOutput(
+            name='Python Live GCS Output',
+            access_key=gcs_output_config.get('accessKey'),
+            secret_key=gcs_output_config.get('secretKey'),
+            bucket=gcs_output_config.get('bucket'),
+            prefix=gcs_output_config.get('prefix'),
+            make_public=False
+        )
+        self.output = bitcodin.create_output(output)
+
+        audio_stream_config = AudioStreamConfig(default_stream_id=0, bitrate=192000)
+        video_stream_config = VideoStreamConfig(default_stream_id=1, bitrate=512000,
+                                                profile='Main', preset='standard', height=480, width=640)
+        encoding_profile = EncodingProfile('API Live Test Profile', [video_stream_config], [audio_stream_config])
+        self.encoding_profile = create_encoding_profile(encoding_profile)
         super(CreateLiveStreamTestCase, self).setUp()
 
     def runTest(self):
-
-        encoding_profiles = bitcodin.list_encoding_profiles()
-        outputs = bitcodin.list_outputs()
-
-        self.assertIsNotNone(encoding_profiles)
-        self.assertIsNotNone(outputs)
-        self.assertGreaterEqual(len(encoding_profiles), 1)
-        self.assertGreaterEqual(len(outputs), 1)
-
-        output = None
-
-        for o in outputs:
-            if o.type == 'gcs':
-                output = o
-                break
-
-        self.assertIsNotNone(output, 'No GCS output was found!')
+        self.assertIsNotNone(self.encoding_profile.encoding_profile_id)
+        self.assertIsNotNone(self.output.output_id)
 
         live_stream = bitcodin.LiveStream("test-live-stream",
                                           "stream",
-                                          encoding_profiles[0].encoding_profile_id,
+                                          self.encoding_profile.encoding_profile_id,
                                           120,
-                                          output.output_id)
+                                          self.output.output_id)
 
         live_stream = bitcodin.create_live_instance(live_stream)
 
