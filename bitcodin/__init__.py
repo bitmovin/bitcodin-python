cd844d874b7b036dd4ff1bdb90e0de89f41710c8
David Moser <david.moser@perau.at>
2016-01-11 09:43:23 +0100

    __init__.py: Fixed wrong api_base url

--- bitcodin/__init__.py
+++ bitcodin/__init__.py
@@ -3,7 +3,7 @@ __author__ = 'dmoser'
 # Configuration variables
 
 api_key = None
-api_base = 'http://portalstaging.bitcodin.com/api'
+api_base = 'https://portal.bitcodin.com/api'
 api_version = None
 
 from bitcodin.resource import *
