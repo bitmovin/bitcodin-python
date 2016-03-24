a662eeea0aaab6d5de7671c25cf7ce1aade3a9ed
David Moser <david.moser@perau.at>
2016-03-24 12:55:38 +0100

    Removed testtools from setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,7 @@ setup(
     include_package_data=True,
     url='https://github.com/bitmovin/bitcodin-python',
     install_requires=[
-        'requests>=2.4.3',
-        'testtools'
+        'requests>=2.4.3'
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
