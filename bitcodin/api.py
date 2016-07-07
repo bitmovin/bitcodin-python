2a81b3dbdf329cdb88764a75eab7010ecbe98ec6
David Moser <david.moser@perau.at>
2016-07-07 16:47:59 +0200

    Added new method get_async_input_status() Wrote a unit test to create an async input and poll the status.

--- bitcodin/api.py
+++ bitcodin/api.py
@@ -36,6 +36,20 @@ def get_input(input_id=None):
     return input_obj
 
 
+def get_async_input_status(async_input_id=None):
+    """
+    Get asynchronous created Input status
+    :param async_input_id: The id of the asynchronous created Input
+    :return: BitcodinObject
+    """
+    url = get_api_base()+'/input/%d' % async_input_id + '/asyncstatus'
+    res = RestClient.get(url=url, headers=create_headers())
+
+    input_obj = BitcodinObject(res, True)
+
+    return input_obj
+
+
 def list_inputs(page=None):
     """
     Get a list of Inputs
